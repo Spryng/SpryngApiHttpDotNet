ba0d61a862aa79983f111e25b6d22574d3239a67
Frank Mand <frankmand@outlook.com>
2016-04-10 14:01:31 +0200

    Example project to show off how to use the API.

---
+++ SpryngApiDotNet.Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spryng;
+
+namespace SpryngApiDotNet.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = GetInput("Username");
+            string password = GetInput("Password", true);
+
+            SpryngClient client = new SpryngClient(username, password);
+
+            Console.WriteLine("Available Credits: {0}", client.GetCreditAmount());
+
+
+
+            Console.ReadKey(true);
+        }
+
+
+        private static string GetInput(string name, bool isHidden = false)
+        {
+            Console.Write("{0}: ", name);
+
+            // If an input type is hidden make sure we do not display the entered characters.
+            if (isHidden)
+            {
+                string result = "";
+                ConsoleKeyInfo keyInfo;
+                while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+                {
+                    result += keyInfo.KeyChar;
+                }
+                Console.WriteLine();
+                return result;
+            }
+
+            return Console.ReadLine();
+        }
+    }
+}
